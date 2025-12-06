814207fd56342cbdded81b7dc4e5c08b5b773026
sadique-habibullah <sadique.habibullah@gmail.com>
2025-12-06 08:34:27 +0600

    test

--- src/site/widgets/custom-elements/testimonial-app/panel.tsx
+++ src/site/widgets/custom-elements/testimonial-app/panel.tsx
@@ -18,6 +18,7 @@ import {
 import Slice from "./panel/slice.js";
 import { ChartType } from "./panel/chart-type.js";
 
+console.log("from sadique");
 const Panel: FC = () => {
   const [loaded, setLoaded] = useState(false);
   const [type, setType] = useState<string>("");
